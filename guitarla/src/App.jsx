1fdf1078ef01cbac999417cf309e278d1756eedf
Ruben Castillo <ruben.castillo@monito.mx>
2024-04-02 12:04:52 -0600

    Última versión funcional del proyecto GuitarLA para carrito de compras

--- guitarla/src/App.jsx
+++ guitarla/src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react"
+import { useState, useEffect } from "react"
 import Guitar from "./components/Guitar"
 import Header from "./components/Header"
 import { db } from './data/db'
@@ -6,12 +6,82 @@ import { db } from './data/db'
 
 function App() {
 
+    const initialCart = () => {
+        const localStorageCart = localStorage.getItem('cart')
+        return localStorageCart ? JSON.parse(localStorageCart) : []
+    }
+
     const [ data, setData ] = useState(db)
-    const [ cart, setCart ] = useState([])
+    const [ cart, setCart ] = useState(initialCart)
+
+    const MAX_ITEMS = 5
+    const MIN_ITEMS = 1
+
+    useEffect(() => {
+        localStorage.setItem('cart', JSON.stringify(cart))
+    }, [cart])
+
+    function addToCart(item) {
+
+        const itemExists =  cart.findIndex(guitar => guitar.id === item.id)
+        if(itemExists >= 0) {
+            if(cart[itemExists].quantity >= MAX_ITEMS) return 
+
+            const updatedCart = [...cart]
+            updatedCart[itemExists].quantity++
+            setCart(updatedCart)
+        } else {
+            item.quantity = 1
+            setCart([...cart, item])
+        }
+    }
+
+    function removeFromCart(id) {
+        setCart(prevCart => prevCart.filter(guitar => guitar.id != id))
+    }
+
+    function increaseQuantity(id) {
+        const updatedCart = cart.map( item => {
+            if(item.id === id && item.quantity < MAX_ITEMS){
+                return {
+                    ...item,
+                    quantity: item.quantity + 1
+                }
+            }
+            return item
+        })
+
+        setCart(updatedCart)
+    }
+
+    function decreaseQuantity(id){
+        const updatedCart = cart.map( item => {
+            if(item.id === id && item.quantity > MIN_ITEMS){
+                return {
+                    ...item,
+                    quantity: item.quantity - 1
+                }
+            }
+            
+            return item
+        })
+
+        setCart(updatedCart)
+    }
+
+    function clearCart(){
+        setCart([])
+    }
 
   return (
     <>
-        <Header />
+        <Header 
+            cart={cart}
+            removeFromCart={removeFromCart}
+            increaseQuantity={increaseQuantity}
+            decreaseQuantity={decreaseQuantity}
+            clearCart={clearCart}
+        />
 
         <main className="container-xl mt-5">
             <h2 className="text-center">Nuestra Colección</h2>
@@ -22,6 +92,8 @@ function App() {
                         <Guitar 
                             key={guitar.id}
                             guitar={guitar}
+                            setCart={setCart}
+                            addToCart={addToCart}
                         />
                     ))
                 }
