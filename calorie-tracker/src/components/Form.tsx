790e3e27b63df4b8e54826d75066e4c5dd232f49
Ruben Castillo <rcastillo@raivtech.com>
2024-04-14 03:51:17 -0600

    Se agrega código de formulario

--- calorie-tracker/src/components/Form.tsx
+++ calorie-tracker/src/components/Form.tsx
@@ -1,6 +1,22 @@
+import { useState, ChangeEvent } from "react"
 import { categories } from "../data/categories"
 
 const Form = () => {
+
+  const [ activity, setActivity] = useState({
+    category: 1,
+    name: '',
+    calories: 0
+  })
+
+  const handleChange = (e : ChangeEvent<HTMLSelectElement> | ChangeEvent<HTMLInputElement>) => {
+    setActivity({
+      ...activity,
+      [e.target.id] : e.target.value
+    })
+
+  }
+
   return (
     <form
       className="space-y-5 bg-white shadow p-10 rounded-lg"
@@ -10,6 +26,8 @@ const Form = () => {
         <select 
           className="border border-slate-300 p-2 rounded-lg w-full bg-white"
           id="category"
+          value={activity.category}
+          onChange={handleChange}
         >
           {categories.map(category => (
             <option 
@@ -23,12 +41,14 @@ const Form = () => {
       </div>
 
       <div className="grid grid-cols-1 gap-3">
-        <label htmlFor="activity" className="font-bold">Actividad:</label>
+        <label htmlFor="name" className="font-bold">Actividad:</label>
         <input 
-          id="activity"
+          id="name"
           type="text"
           className="border border-slate-300 p-2 rounded-lg"
           placeholder="Ej. Comida, Jugo de Naranja, Ensalada, Ejercicio, Pesas, Bicicleta"
+          value={activity.name}
+          onChange={handleChange}
         />
       </div>
 
@@ -39,6 +59,8 @@ const Form = () => {
           type="number"
           className="border border-slate-300 p-2 rounded-lg"
           placeholder="Calorías. Ej. 300 o 500"
+          value={activity.calories}
+          onChange={handleChange}
         />
       </div>
 
