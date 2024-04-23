548e74b93cdad3ac85deab0908bca82bb5ba03bc
Ruben Castillo <rcastillo@raivtech.com>
2024-04-23 17:46:40 -0600

    Se agregan useReducer al proyecto y se elimina el hook useOrder

---
+++ calculadora_propinas-usereducer/src/App.tsx
@@ -0,0 +1,61 @@
+import MenuItem from "./components/MenuItem"
+import { menuItems } from "./data/db"
+import OrderContents from "./components/OrderContents"
+import OrderTotals from "./components/OrderTotals"
+import TipPercentajeForm from "./components/TipPercentajeForm"
+import { useReducer } from "react"
+import { initialState, orderReducer } from "./reducers/order-reducer"
+
+function App() {
+
+  const [ state, dispatch ] = useReducer(orderReducer, initialState)
+
+
+  return (
+    <>
+      <header className="bg-teal-400 py-5">
+        <h1 className="text-center text-4xl font-black">Calculadora de propinas y consumo</h1>
+      </header>
+
+      <main className="max-w-7xl mx-auto py-20 grid md:grid-cols-2">
+        <div className="p-5">
+          <h2 className="text-4xl font-black">Menú</h2>
+          <div className="space-y-3 mt-10">
+            {menuItems.map(item => (
+              <MenuItem 
+                key={item.id}
+                item={item}
+                dispatch={dispatch}
+              />
+            ))}
+          </div>
+        </div>
+
+        <div className="border border-dashed border-slate-300 p-5 rounded-lg space-y-10">
+          {state.order.length ? (
+            <>
+              <OrderContents 
+                order={state.order}
+                dispatch={dispatch}
+              />
+              <TipPercentajeForm 
+                dispatch={dispatch}
+                tip={state.tip}
+              />
+              <OrderTotals 
+                order={state.order}
+                tip={state.tip}
+                dispatch={dispatch}
+              />
+            </>
+          ) : (
+            <p className="text-center">La orden está vacía</p>
+          )}
+          
+        </div>
+      </main>
+    </>
+  )
+}
+
+export default App
---
+++ calculadora_propinas-usereducer/src/components/MenuItem.tsx
@@ -0,0 +1,21 @@
+import { Dispatch } from "react"
+import { OrderActions } from "../reducers/order-reducer"
+import type { MenuItem } from "../types"
+
+type MenuItemProps = {
+  item: MenuItem
+  dispatch: Dispatch<OrderActions>
+}
+
+export default function MenuItem({item, dispatch} : MenuItemProps) {
+
+  return (
+    <button
+      className="border-2 border-teal-400 hover:bg-teal-200 w-full p-3 flex justify-between rounded"
+      onClick={() => dispatch({type: 'add-item', payload: {item}})}
+    >
+      <p>{item.name}</p>
+      <p className="font-black">${item.price}</p>
+    </button>
+  )
+}
