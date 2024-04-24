d71917c23c639583f10f7b27e5964958fb671a69
Ruben Castillo <rcastillo@raivtech.com>
2024-04-23 21:29:43 -0600

    Se agrega Context y custom hooks para manejar el budget

---
+++ control-gastos-contextapi/src/components/BudgetForm.tsx
@@ -0,0 +1,43 @@
+import { useState, ChangeEvent, useMemo } from "react"
+
+const BudgetForm = () => {
+
+    const [ budget, setBudget ] = useState(0)
+
+    const handleChange = (e :ChangeEvent<HTMLInputElement>) => {
+        const budgetValue = !isNaN(e.target.valueAsNumber) ? e.target.valueAsNumber : 0
+        setBudget(budgetValue)
+    }
+
+    const isValid = useMemo(() => {
+        return isNaN(budget) || budget <= 0
+    } ,[budget])
+
+
+    return (
+        <form className="space-y-5">
+            <div className="flex flex-col space-y-5">
+                <label htmlFor="budget" className="text-4xl text-blue-600 font-bold text-center">
+                    Definir presupuesto
+                </label>
+                <input 
+                    id="budgetId"
+                    type="number"
+                    className="w-full bg-white border border-gray-200 p-2"
+                    placeholder="Define tu presupuesto"
+                    name="budget"
+                    value={budget}
+                    onChange={handleChange}
+                />
+            </div>
+            <input
+                type="submit"
+                value="Definir Presupuesto"
+                className="w-full bg-blue-600 hover:bg-blue-700 cursor-pointer p-2 text-white font-black uppercase rounded disabled:opacity-40"
+                disabled={isValid}
+            />
+        </form>
+    )
+}
+
+export default BudgetForm
