514c6eb6e17ff206956a6a9b32cf218bacff782c
Ruben Castillo <rcastillo@raivtech.com>
2024-04-29 19:53:58 -0600

    Se trabaja en el reducer y el form del proyecto

--- control-gastos-contextapi/src/reducers/budget-reducer.ts
+++ control-gastos-contextapi/src/reducers/budget-reducer.ts
@@ -1,13 +1,30 @@
+import { v4 as uuidv4 } from 'uuid'
+import { Expense, DraftExpense } from "../components/types"
 
 export type BudgetActions = 
-    { type: 'add-budget', payload: { budget: number }} 
+    { type: 'add-budget', payload: { budget: number }} |
+    { type: 'show-modal' } |
+    { type: 'close-modal' } |
+    { type: 'add-expense', payload: { expense: DraftExpense }}
 
 export type BudgetState = {
     budget: number
+    modal: boolean,
+    expenses: Expense[]
 }
 
 export const initialState : BudgetState = {
-    budget: 0
+    budget: 0,
+    modal: false,
+    expenses: []
+}
+
+const createExpense = (draftExpense: DraftExpense) : Expense => {
+    return {
+        ...draftExpense,
+        id: uuidv4()
+
+    }
 }
 
 export const budgetReducer = (
@@ -18,7 +35,32 @@ export const budgetReducer = (
     if(action.type === 'add-budget') {
         return {
             ...state,
-            bufget: action.payload.budget
+            budget: action.payload.budget
+        }
+    }
+
+    if(action.type === 'show-modal') {
+        return {
+            ...state,
+            modal: true
+        }
+    }
+
+    if(action.type === 'close-modal') {
+        return {
+            ...state,
+            modal: false
+        }
+    }
+
+    if(action.type === 'add-expense') { 
+        const expense = createExpense(action.payload.expense)
+
+        return {
+            ...state,
+            expenses: [...state.expenses, expense],
+            modal: false
+
         }
     }
 
